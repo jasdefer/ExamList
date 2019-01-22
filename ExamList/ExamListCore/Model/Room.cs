5528f86d0cc8128e40a2922e2260efd59b82e1f2
Justus Bonz <justus.bonz@gmail.com>
2019-01-22 09:10:30 +0100

    Added model

---
+++ ExamList/ExamListCore/Model/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExamListCore.Model
+{
+    public class Room
+    {
+        public Room(string name, int capacity)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            if (capacity < 1) throw new ArgumentOutOfRangeException(nameof(capacity));
+            Capacity = capacity;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Capacity})";
+        }
+    }
+}
---
+++ ExamList/ExamListCore/Model/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace ExamListCore.Model
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string DegreeCourse { get; set; }
+        public decimal BonusPoints { get; set; }
+        public int Seat { get; private set; }
+        public Room Room { get; private set; }
+
+        public Student(int id)
+        {
+            StudentId = id;
+        }
+
+        public void SetSeat(Room room, int seat)
+        {
+            Room = room;
+            Seat = seat;
+        }
+    }
+}
