ae7b3d0641dd3b4f57a58a85dfa3d31bbac42ad2
Justus Bonz <justus.bonz@gmail.com>
2019-01-24 11:18:47 +0100

    Added package information

--- ExamList/ExamListConsole/App.cs
+++ ExamList/ExamListConsole/App.cs
@@ -1,18 +1,23 @@
 ﻿using ExamListCore;
+using Microsoft.Extensions.Logging;
 
 namespace ExamListConsole
 {
     public class App
     {
         private readonly ExamListManager examListManager;
+        private readonly ILogger<ExamListManager> logger;
 
-        public App(ExamListManager examListManager)
+        public App(ExamListManager examListManager,
+            ILogger<ExamListManager> logger)
         {
             this.examListManager = examListManager;
+            this.logger = logger;
         }
 
         public void Run()
         {
+            logger.LogInformation("Welcome to the exam list manager.");
             examListManager.DistributeStudents();
         }
     }
